09dbd31131ee7ea2990b3d9232695d2e277f9ad2
jessica-dds <jessica.gleizer@hotmail.com>
2023-12-04 20:57:11 -0300

    Funções com parâmetros

---
+++ funcoes-com-parametros.js
@@ -0,0 +1,8 @@
+function cumprimentar(pessoa, idade) {
+    console.log(`Olá, ${pessoa}! Me contaram que você tem ${idade} anos.`);
+}
+
+cumprimentar("Jéssica", 35);
+cumprimentar("Maria", 40);
+cumprimentar("Caio", 15);
+cumprimentar("Angelo", 90);
