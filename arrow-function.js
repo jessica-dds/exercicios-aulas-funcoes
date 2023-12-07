6f80b071e2e30e132b1a753fead6e48b5906c944
jessica-dds <jessica.gleizer@hotmail.com>
2023-12-06 21:12:43 -0300

    arrow function

---
+++ arrow-function.js
@@ -0,0 +1,16 @@
+
+// declaração tradicional
+//function soma(x, y) {
+//código
+//const resultado = x + y;
+// return resultado;
+//}
+
+//declaração arrow-function
+const soma = (x, y) => {
+    //código
+    const resultado = x + y;
+    return resultado;
+}
+
+console.log(soma(5, 2));
