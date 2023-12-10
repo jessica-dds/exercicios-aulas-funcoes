178ab567f6d3e7b45ffa3065f75d2e287d005271
jessica-dds <jessica.gleizer@hotmail.com>
2023-12-10 14:52:05 -0300

    ex.5

---
+++ ex5.js
@@ -0,0 +1,11 @@
+//console.log é um método
+// método nativo do java
+
+const console = {
+    num_de_caracteres: 40,
+    log: function (texto) {
+        //fez o código que imprime as coisas no terminal
+    }
+}
+
+console.log("Texto para imprmir");
